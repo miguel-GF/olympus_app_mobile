0db79ca0785567f814c4c31a9d845dd5170dfa73
Miguel <miguel.gf.130@gmail.com>
2025-02-02 00:22:32 -0600

    feat: Se agrega validación básica y controllers para datos de logeo

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,20 +17,35 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool _isLoading = false;
+  final TextEditingController usuarioTextCtr = TextEditingController();
+  final TextEditingController passwordTextCtr = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    usuarioTextCtr.dispose();
+    passwordTextCtr.dispose();
+    super.dispose();
+  }
+
   Future<void> _irHome() async {
     // Get.offAndToNamed(nameTabsScreen);
     try {
       setState(() {
         _isLoading = true;
       });
-      // ignore: always_specify_types
-      await Future.delayed(const Duration(seconds: 1));
-      final UsuarioController usuarioController = Get.find<UsuarioController>();
-      await usuarioController.buscar(
-        usuario: 'demo@gmail.com',
-        password: '1234',
-      );
-      await usuarioController.listar();
+      if (formKey.currentState?.validate() ?? false) {
+        final String usuario = usuarioTextCtr.text.trim();
+        final String password = passwordTextCtr.text.trim();
+        // ignore: always_specify_types
+        await Future.delayed(const Duration(milliseconds: 500));
+        final UsuarioController usuarioController =
+            Get.find<UsuarioController>();
+        await usuarioController.buscar(
+          usuario: usuario,
+          password: password,
+        );
+      }
     } catch (e) {
       print('desde login');
       print(e);
@@ -52,35 +67,59 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Center(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const SizedBox(child: GbInputLabel(texto: 'Usuario')),
-                  TextFormField(),
-                  const MaxGap(30),
-                  const GbInputLabel(texto: 'Password'),
-                  TextFormField(),
-                  const MaxGap(30),
-                  AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 350),
-                    transitionBuilder:
-                        (Widget child, Animation<double> animation) {
-                      return FadeTransition(
-                        opacity: animation,
-                        child: ScaleTransition(scale: animation, child: child),
-                      );
-                    },
-                    child: !_isLoading
-                        ? GbButton(
-                            texto: 'Login',
-                            onPressed: _irHome,
-                          )
-                        : LoadingAnimationWidget.dotsTriangle(
-                            color: Theme.of(context).primaryColor,
-                            size: 50,
-                          ),
-                  ),
-                ],
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const SizedBox(child: GbInputLabel(texto: 'Usuario')),
+                    TextFormField(
+                      controller: usuarioTextCtr,
+                      textInputAction: TextInputAction.next,
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return 'Tu usuario es requerido';
+                        }
+                        return null;
+                      },
+                    ),
+                    const MaxGap(30),
+                    const GbInputLabel(texto: 'Password'),
+                    TextFormField(
+                      controller: passwordTextCtr,
+                      textInputAction: TextInputAction.done,
+                      obscureText: true,
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return 'Tu password es requerido';
+                        }
+                        return null;
+                      },
+                      onFieldSubmitted: (_) => _irHome(),
+                    ),
+                    const MaxGap(30),
+                    AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 350),
+                      transitionBuilder:
+                          (Widget child, Animation<double> animation) {
+                        return FadeTransition(
+                          opacity: animation,
+                          child:
+                              ScaleTransition(scale: animation, child: child),
+                        );
+                      },
+                      child: !_isLoading
+                          ? GbButton(
+                              texto: 'Login',
+                              onPressed: _irHome,
+                            )
+                          : LoadingAnimationWidget.dotsTriangle(
+                              color: Theme.of(context).primaryColor,
+                              size: 50,
+                            ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
