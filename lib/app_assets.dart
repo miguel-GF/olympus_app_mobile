e62cbdd31cbb53afc6af2080c15d269c6318faf5
Miguel <miguel.gf.130@gmail.com>
2025-02-20 01:02:58 -0600

    chore(assets): Se agregan logos de app

--- lib/app_assets.dart
+++ lib/app_assets.dart
@@ -1,4 +1,6 @@
 class AppAssets {
   static String wifiAnimation = 'assets/animations/wifi.json';
   static String loadingAnimation = 'assets/animations/loading.json';
+  static String logoAppImage = 'assets/images/logo_app.jpeg';
+  static String logoEmpresaImage = 'assets/images/logo_empresa.jpeg';
 }
